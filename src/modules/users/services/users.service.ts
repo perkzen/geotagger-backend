37fecdeab155ed126717a33aa2fb69b85ccab59c
perkzen <perko.domen@gmail.com>
2024-06-30 21:01:36 +0200

    fix: imports

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from '@app/modules/users/dtos/create-user.dto';
 import { CannotCreateUserException } from '@app/modules/users/exceptions/cannot-create-user.exception';
-import { UserNotFoundException } from '@app/modules/users/exceptions/user-not-found.exception';
 import { UsersRepository } from '@app/modules/users/repositories/users.repository';
 
 @Injectable()
