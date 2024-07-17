3f5300c1f0927c499f32856b7f0501faf0ae31fa
perkzen <perko.domen@gmail.com>
2024-07-17 08:21:59 +0200

    fix: import

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,4 +1,4 @@
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '@app/modules/auth/decorators/public.decorator';
