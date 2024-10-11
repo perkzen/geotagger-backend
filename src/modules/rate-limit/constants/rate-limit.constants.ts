2bae6c03b123a2588cd467b6091708d2091d8663
perkzen <perko.domen@gmail.com>
2024-10-11 09:21:09 +0200

    fix: rate limits

--- src/modules/rate-limit/constants/rate-limit.constants.ts
+++ src/modules/rate-limit/constants/rate-limit.constants.ts
@@ -2,30 +2,21 @@ import { ThrottlerOptions, minutes, seconds } from '@nestjs/throttler';
 import { RateLimitName } from '@app/modules/rate-limit/enums/rate-limit-name.enum';
 
 /**
- * 5 requests per second
+ * 30 requests per second
  */
 export const SHORT_RATE_LIMIT: ThrottlerOptions = {
   name: RateLimitName.SHORT,
   ttl: seconds(30),
-  limit: 5,
+  limit: 30,
 };
 
 /**
- * 25 requests per 30 seconds
- */
-export const MEDIUM_RATE_LIMIT: ThrottlerOptions = {
-  name: RateLimitName.MEDIUM,
-  ttl: seconds(30),
-  limit: 25,
-};
-
-/**
- * 50 requests per minute
+ * 60 requests per minute
  */
 export const LONG_RATE_LIMIT: ThrottlerOptions = {
   name: RateLimitName.LONG,
   ttl: minutes(1),
-  limit: 50,
+  limit: 60,
 };
 
 /**
